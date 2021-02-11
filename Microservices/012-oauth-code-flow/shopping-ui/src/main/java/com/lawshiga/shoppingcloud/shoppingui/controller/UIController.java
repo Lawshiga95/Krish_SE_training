be75980431d6c068d39b7de7ea6c87e355dcd1b2
Lawshiga95 <lawshiga123@gmail.com>
2021-02-11 20:07:10 +0530

    mvcMatcher renamed as antMatcher

--- Microservices/012-oauth-code-flow/shopping-ui/src/main/java/com/lawshiga/shoppingcloud/shoppingui/controller/UIController.java
+++ Microservices/012-oauth-code-flow/shopping-ui/src/main/java/com/lawshiga/shoppingcloud/shoppingui/controller/UIController.java
@@ -13,7 +13,7 @@ public class UIController extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/")
+                .antMatchers("/")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
